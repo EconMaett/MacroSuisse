04306ce51cc56e9403184b86d9af2eab464d94e2
EconMaett <matthias.spichiger@bluewin.ch>
2023-09-15 13:54:39 +0200

    Initial commit

--- R/calcIndex.R
+++ R/calcIndex.R
@@ -3,9 +3,12 @@ calcIndex <- function(series, weights, baseY) {
   
   series <- ts_index(series, baseY) * 100
   
-  Index <- sapply(X = seq_len(nrow(series)), FUN = function(i) {
-    weighted.mean(x = as.matrix(series[i, ]), w = weights, na.rm = TRUE)
-  })
+  Index <- sapply(
+    X = seq_len(nrow(series)), 
+    FUN = function(i) {
+      weighted.mean(x = as.matrix(series[i, ]), w = weights, na.rm = TRUE)
+      }
+    )
   
   Index <- xts(Index, order.by = as.Date(index(series)))
   
